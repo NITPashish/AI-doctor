258dc5811098dfbcd2c403b7e925d0fd551c6772
NITPashish <ashishr.ug20.me@nitp.ac.in>
2025-12-05 00:31:16 +0530

    clear chat implemented

--- app.py
+++ app.py
@@ -31,19 +31,38 @@ vectorData = PineconeVectorStore.from_existing_index(
 
 retriever = vectorData.as_retriever(search_type = "similarity", search_kwargs = {"k": 3})
 
+from langchain.memory import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+store = {}
+def get_session_history(session_id: str):
+  if session_id not in store:
+    store[session_id]=ChatMessageHistory()
+  return store[session_id]
+
+
+
+  
+
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 prompt = ChatPromptTemplate.from_messages(
   [
-    ("system", system_prompt),
+    ("system", system_prompt + "\n\n{chat_history}"),
     ("human", "{input}"),
   ]
 )
 QA_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, QA_chain)
 
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer"
+)
 
 
 # Mount static folder
@@ -57,7 +76,13 @@ async def home(request: Request):
   return templates.TemplateResponse("chatbot.html", {"request": request})
 
 @app.post("/get")
-def get_response(msg: str = Form(...)):
-  response = rag_chain.invoke({"input": msg})
+def get_response(msg: str = Form(...), session_id: str = Form(...)):
+  
+  response = conversational_rag_chain.invoke({"input": msg},config={"configurable": {"session_id": session_id}})
   return str(response["answer"])
+
+@app.post("/clear")
+def clear_chat(session_id: str = Form(...)):
+    store[session_id] = ChatMessageHistory()
+    return {"status": "cleared"}
   
